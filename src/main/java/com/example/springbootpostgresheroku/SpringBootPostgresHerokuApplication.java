9eebda3dce693601038bb0cdf04e056d6f47abf5
Michael <michaelx@oriental.com.au>
2021-12-10 11:17:19 +1100

    added application dev properties

--- src/main/java/com/example/springbootpostgresheroku/SpringBootPostgresHerokuApplication.java
+++ src/main/java/com/example/springbootpostgresheroku/SpringBootPostgresHerokuApplication.java
@@ -1,13 +1,26 @@
 package com.example.springbootpostgresheroku;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPostgresHerokuApplication {
+public class SpringBootPostgresHerokuApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootPostgresHerokuApplication.class);
+
+    @Value("${testing}")
+    private String testing;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPostgresHerokuApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("testing {}", testing);
+    }
 }
